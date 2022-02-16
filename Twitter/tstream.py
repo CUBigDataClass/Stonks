495f6258f39eb616223ed1c7db0b944185afe09a
NikolaiLyssogor <59844691+NikolaiLyssogor@users.noreply.github.com>
2022-02-15 17:58:14 -0700

    filter desired data from API

--- Twitter/tstream.py
+++ Twitter/tstream.py
@@ -1,16 +1,27 @@
-import tweepy
+from tweepy import Stream
+from json import loads
 from keys import *
 
-# Create a StreamListener
-class TwitterStream(tweepy.Stream):
+class TwitterStream(Stream):
+    """
+    Inherits from tweepy's Stream class. We are only modifying the few functions
+    we need to customize, namely what to do when new tweets come through.
+    """
 
     def on_data(self, raw_data):
+        """
+        Gets called every time a new tweet gets filtered through.
+        """
         self.process_data(raw_data)
 
         return True
 
     def process_data(self, raw_data):
-        print(raw_data)
+        response = loads(raw_data)
+
+        print(response['text'])
+        print(f'followers: {response["user"]["followers_count"]}')
+        print(f'timestamp: {response["created_at"]}\n')
 
     def on_error(self, status_code):
         # Returning false disconnects the stream
@@ -25,7 +36,4 @@ if __name__ == "__main__":
         ACCESS_TOKEN, ACCESS_TOKEN_SECRET
     )
 
-    stream.filter(track=['python'])
-
-
-
+    stream.filter(track=['Microsoft'], languages=['en'])
