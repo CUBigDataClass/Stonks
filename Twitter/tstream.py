e71019e39f3a3c365938f0f9e5f9047d26e77931
Nikolai Lyssogor <lyssogor@gmail.com>
2022-03-03 18:02:22 -0700

    add company name to twitter object

--- Twitter/tstream.py
+++ Twitter/tstream.py
@@ -1,8 +1,8 @@
 from tweepy import Stream
 import json
 from keys import *
+from companies import *
 from kafka import KafkaProducer
-
 
 class TwitterStream(Stream):
     """
@@ -10,10 +10,18 @@ class TwitterStream(Stream):
     we need to customize, namely what to do when new tweets come through.
     """
 
-    # Kafka definition
-    TOPIC_NAME = 'stock_data'
-    KAFKA_SERVER = 'localhost:9092'
-    producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    def get_companies(self, tweet):
+        """
+        :param tweet: The string text of a tweet.
+        :return: A list of companies mentioned in the tweet.
+        """
+        tweet = tweet.lower()
+        companies_mentioned = []
+        for stock in stocks:
+            if stock in tweet:
+                companies_mentioned.append(stock)
+
+        return companies_mentioned
 
     def on_data(self, raw_data):
         """
@@ -28,7 +36,18 @@ class TwitterStream(Stream):
         Sends data through the Kafka log.
         """
         response = json.loads(raw_data)
-        producer.send(TOPIC_NAME, response)
+
+        # Get tweet text
+        if 'extended_tweet' in response:
+            tweet_text = response['extended_tweet']['full_text']
+        else:
+            tweet_text = response['text']
+
+        # Associate each company mentioned with the tweet
+        companies_mentioned = self.get_companies(tweet_text)
+        for corp in companies_mentioned:
+            response['company_name'] = corp
+            producer.send(TOPIC_NAME, response)
 
     def on_error(self, status_code):
         """
@@ -39,6 +58,7 @@ class TwitterStream(Stream):
 
 # Start the Stream
 if __name__ == "__main__":
+    # Configuration for Kafka
     TOPIC_NAME = 'stock_data'
     KAFKA_SERVER = 'localhost:9092'
     producer = KafkaProducer(
@@ -51,7 +71,7 @@ if __name__ == "__main__":
         API_KEY, API_KEY_SECERT,
         ACCESS_TOKEN, ACCESS_TOKEN_SECRET
     )
-    print("producer is running...")
+    print("producer is producing...")
     stream.filter(track=['Microsoft'], languages=['en'])
     producer.flush()
 
