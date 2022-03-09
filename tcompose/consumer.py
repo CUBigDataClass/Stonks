8a785619416a3b64306319862d9ac6faf09191ac
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-03-09 15:24:00 -0700

    consumer merge

--- tcompose/consumer.py
+++ tcompose/consumer.py
@@ -6,6 +6,8 @@ from json import dumps
 from json import loads
 from multiprocessing import Process
 from keys import *
+import threading
+import time
 
 """
 To DO:
@@ -17,53 +19,74 @@ problems: might need better api
     if we are using GCP figure out what port to use for kafka_server
     possible candidates yahoofinance, google finance.
     could we possibly pull historical data from alphadvantage and pull more real time from yahoo/google?
-    
+
 """
 
-TOPIC_NAME = 'stock_data'
-#KAFKA_SERVER = 'kafka:9092'
+# TOPIC_NAME = 'stock_data'
+# TOPIC_NAME = 'news'
+TOPIC_NAME = 'stock'
+# KAFKA_SERVER = 'kafka:9092'
 KAFKA_SERVER = 'kafka-1:9092'
-#KAFKA_SERVER = 'kafka:9093'
-#KAFKA_SERVER = 'localhost:9092'
-#KAFKA_SERVER = 'localhost:9093'
-#KAFKA_SERVER = '0.0.0.0:9092's
-#KAFKA_SERVER= 'localhost:29092'
-#consumer=KafkaConsumer(TOPIC_NAME, bootstrap_servers=[KAFKA_SERVER], value_deserializer=lambda m:loads(m.decode('utf-8')))
-#now=datetime.now()
-#daysago=3
-#past_time=now-timedelta(days=3)
-#current_time=past_time.strftime("%H:%M:00")
-#current_date=past_time.strftime("%Y-%m-%d")
+# KAFKA_SERVER = 'kafka:9093'
+# KAFKA_SERVER = 'localhost:9092'
+# KAFKA_SERVER = 'localhost:9093'
+# KAFKA_SERVER = '0.0.0.0:9092's
+# KAFKA_SERVER= 'localhost:29092'
+# TOPIC_LIST= ['stock', 'news']
+consumer = KafkaConsumer(bootstrap_servers=[KAFKA_SERVER], value_deserializer=lambda m: loads(m.decode('utf-8')))
+# consumer.subscribe(TOPIC_NAME)
+consumer.subscribe(['stock', 'news'])
+# consumer.subscribe(TOPIC_LIST)
+# consumer.subscribe('news')
+# now=datetime.now()
+# daysago=3
+# past_time=now-timedelta(days=3)
+# current_time=past_time.strftime("%H:%M:00")
+# current_date=past_time.strftime("%Y-%m-%d")
+
+"""
 def stock_consumer():
     TOPIC_NAME = 'stock_data'
-    # KAFKA_SERVER = 'kafka:9092'
-    # KAFKA_SERVER = 'kafka:9093'
+
     KAFKA_SERVER = 'kafka-1:9092'
     consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=[KAFKA_SERVER],value_deserializer=lambda m: loads(m.decode('utf-8')))
     for message in consumer:
         print(1)
-        print(message)
+        time.sleep(1)
+        #print(message)
         #put new data in to cold storage
         #parse the new data put it in to postgres
-
 def news_consumer():
     TOPIC_NAME='news'
     KAFKA_SERVER = 'kafka-1:9092'
     consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=[KAFKA_SERVER],value_deserializer=lambda m: loads(m.decode('utf-8')))
     for message in consumer:
         print(2)
-        print(message)
-
+        time.sleep(1)
+        #print(message)
 """
+
 for message in consumer:
-    #print(message[6]["Time Series (Daily)"][current_date])
-    #print(message[6]["Time Series (Daily)"])
+    # print(message[6]["Time Series (Daily)"][current_date])
+    # print(message[6]["Time Series (Daily)"])
     print(message)
+    if (message[0] == 'stock'):
+        print("russia")
+    elif (message[0] == 'news'):
+        print("news")
+
+    # print(message[0])
+    # print(message[1])
 """
 if __name__ == '__main__':
+    thread1 = threading.Thread(target=stock_consumer)
+    thread1.start()
+    thread2 = threading.Thread(target=news_consumer)
+    thread2.start()
+
     Process(target=stock_consumer()).start()
     Process(target=news_consumer()).start()
-"""
-parse the info further and put it in to storage
 
 """
+# parse the info further and put it in to storage
+
