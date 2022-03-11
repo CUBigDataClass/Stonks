7cb2c6aab6bfd17eb7df82501fccb666f9d5c37e
NikolaiLyssogor <59844691+NikolaiLyssogor@users.noreply.github.com>
2022-03-10 18:11:38 -0700

    connect consumer.py to postgres

--- Twitter/consumer.py
+++ Twitter/consumer.py
@@ -1,6 +1,22 @@
 from json import loads, dumps
 from google.cloud import storage
 from kafka import KafkaConsumer
+from google.cloud.sql.connector import connector
+import pg8000.native
+import sqlalchemy
+
+def getconn() -> pg8000.native.Connection:
+    """
+    :return: Connection to the 'stonks' db on the GCP Postgres instance
+    """
+    conn: pg8000.native.Connection = connector.connect(
+        "stonks-343022:us-central1:stock-stack",
+        "pg8000",
+        user="postgres",
+        password="darpatliznikjuschi",
+        db="stonks"
+    )
+    return conn
 
 # kafka configuration
 TOPIC_NAME = 'stock_data'
@@ -15,8 +31,23 @@ storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(destination_blob_name)
 
-# upload data from the stream to the bucket
-for message in consumer:
-    tweet = dumps(message.value)
-    blob.upload_from_string(tweet)
-    
+# get pooled connection to cloud postgres instance
+pool = sqlalchemy.create_engine(
+    "postgresql+pg8000://",
+    creator=getconn,
+)
+
+# upload data from the stream to the bucket and postgres
+with pool.connect() as db_conn:
+    for message in consumer:
+        # get tweet as json
+        tweet = dumps(message.value)
+
+        # stream to the storage bucket
+        blob.upload_from_string(tweet)
+
+        # get sentiment score for the tweet
+        # DO SENTIMENT ANALYSIS HERE
+
+        # insert into postgres db
+        # INSERT STATEMENT GOES HERE
