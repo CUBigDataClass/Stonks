1fab6edaf3eabe10c72957bd5bbf331eef6a4b62
Nikolai Lyssogor <lyssogor@gmail.com>
2022-04-07 17:46:27 -0600

    finished consumer-side sentiment

--- Twitter/consumer.py
+++ Twitter/consumer.py
@@ -6,18 +6,18 @@ import pg8000.native
 import sqlalchemy
 from sentiment import *
 
-def getconn() -> pg8000.native.Connection:
-    """
-    :return: Connection to the 'stonks' db on the GCP Postgres instance
-    """
-    conn: pg8000.native.Connection = connector.connect(
-        "stonks-343022:us-central1:stock-stack",
-        "pg8000",
-        user="postgres",
-        password="darpatliznikjuschi",
-        db="stonks"
-    )
-    return conn
+# def getconn() -> pg8000.native.Connection:
+#     """
+#     :return: Connection to the 'stonks' db on the GCP Postgres instance
+#     """
+#     conn: pg8000.native.Connection = connector.connect(
+#         "stonks-343022:us-central1:stock-stack",
+#         "pg8000",
+#         user="postgres",
+#         password="darpatliznikjuschi",
+#         db="stonks"
+#     )
+#     return conn
 
 
 # kafka configuration
@@ -27,31 +27,55 @@ consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers = KAFKA_SERVER,
                          value_deserializer = lambda m : loads(m.decode('utf-8')))
 
 # google cloud storage configuration
-bucket_name = 'stonks_chilly_storage'
-destination_blob_name = 'twitter_stream'
-storage_client = storage.Client()
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(destination_blob_name)
+# bucket_name = 'stonks_chilly_storage'
+# destination_blob_name = 'twitter_stream'
+# storage_client = storage.Client()
+# bucket = storage_client.bucket(bucket_name)
+# blob = bucket.blob(destination_blob_name)
 
 # get pooled connection to cloud postgres instance
-pool = sqlalchemy.create_engine(
-    "postgresql+pg8000://",
-    creator=getconn,
-)
+# pool = sqlalchemy.create_engine(
+#     "postgresql+pg8000://",
+#     creator=getconn,
+# )
 
 # upload data from the stream to the bucket and postgres
-with pool.connect() as db_conn:
-    for message in consumer:
-        # get tweet as json
-        tweet = dumps(message.value)
+# with pool.connect() as db_conn:
+for message in consumer:
+    # get tweet as json
+    tweet_object = dumps(message.value)
 
-        # stream to the storage bucket
-        blob.upload_from_string(tweet)
+    # stream to the storage bucket
+    # blob.upload_from_string(tweet_object)
 
-        # get sentiment score for the tweet
-        tweet_sentiment = SentimentAnalyzer(tweet['text'])
-        tweet_sentiment.run_sentiment_analysis()
-        sentiment_scores = ???
+    # get text of the tweet
+    if 'extended_tweet' in message.value:
+        tweet_text = message.value['extended_tweet']['full_text']
+    else:
+        tweet_text = message.value['text']
 
-        # insert into postgres db
-        # INSERT STATEMENT GOES HERE
+    # get sentiment score for the tweet
+    tweet_sentiment = SentimentAnalyzer(tweet_text)
+    tweet_sentiment.run_sentiment_analysis()
+    sentiment_scores = tweet_sentiment.get_sentiment_score()
+
+    # discretize compound sentiment score
+    compound_sentiment = sentiment_scores['compound']
+
+    if compound_sentiment == 0.0:
+        sentiment_class = 'neutral'
+    elif compound_sentiment < 0.0:
+        sentiment_class = 'negative'
+    else:
+        sentiment_class = 'positive'
+
+    no_followers = message.value['user']['followers_count']
+
+    # print stuff that stored in Postgres
+    # print(tweet_text)
+    # print(f"compound sentiment score: {compound_sentiment}")
+    # print(f'sentiment class: {sentiment_class}')
+    # print(f"no. followers: {no_followers}")
+
+    # insert into postgres db
+    # INSERT STATEMENT GOES HERE
--- Twitter/sentiment.py
+++ Twitter/sentiment.py
@@ -2,8 +2,6 @@ import pandas as pd
 import re
 import string
 import nltk
-import seaborn as sns
-sns.set_style('darkgrid')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -15,30 +13,31 @@ warnings.filterwarnings("ignore")
 class SentimentAnalyzer:
 
     def __init__(self, tweet_text):
-        self.df = pd.DataFrame(???)
+        self.df = pd.DataFrame([tweet_text], columns=['Tweet'])
+        self.sentiment_scores = None
 
-    def make_dataframe(self, responses):
-        #Takes the text of the tweet and converts it into a dataframe.
-        self.df = self.df.append(pd.Series(responses, index=df.columns[:len(responses)]), ignore_index=True)
+    # def make_dataframe(self, responses):
+    #     #Takes the text of the tweet and converts it into a dataframe.
+    #     self.df = self.df.append(pd.Series(responses, index=df.columns[:len(responses)]), ignore_index=True)
 
 
-    def url_removal(self, df):
+    def url_removal(self):
         #removes the url from the tweet
         self.df['Tweet'] = self.df['Tweet'].apply(lambda x:re.sub(r"http\S+", "", x))
 
-    def punctuation_removal(self, df):
+    def punctuation_removal(self):
         #Removing of the Punctuations from the tweet's text
         punctuation_removal = string.punctuation
         def remove_punctuation(text):
             return text.translate(str.maketrans('', '', punctuation_removal))
         self.df['Tweet'] = self.df['Tweet'].apply(lambda text: remove_punctuation(text))
 
-    def single_and_double_character_space_removal(self, df):
+    def single_and_double_character_space_removal(self):
         #Removal of Single and Double character space from the tweet's text
         self.df['Tweet'] = self.df['Tweet'].apply(lambda x:re.sub(r'\s+[a-zA-Z]\s+', '', x))
         self.df['Tweet'] = self.df['Tweet'].apply(lambda x:re.sub(r'\s+', ' ', x, flags=re.I))
 
-    def stopwords_removal(self,df):
+    def stopwords_removal(self):
         #Removing Stop words from the tweet like ["like", "such", "a"]
         #NLTK has a predefined list of stopwords which makes it easy to just remove them from the tweet's text.
         STOPWORDS = set(stopwords.words('english'))
@@ -46,7 +45,7 @@ class SentimentAnalyzer:
             return " ".join([word for word in str(text).split() if word not in STOPWORDS])
         self.df['Tweet'] = self.df['Tweet'].apply(lambda text: remove_stopwords(text))
 
-    def remove_emoji(self, df):
+    def remove_emoji(self):
         #Removal of Emoji's from the tweet's text
         def emoji(string):
             emoji_pattern = re.compile("["
@@ -73,7 +72,7 @@ class SentimentAnalyzer:
         self.df['Tweet'] = self.df['Tweet'].apply(str)
         self.df['Tweet'] = self.df['Tweet'].apply(emoji)
 
-    def tokenization(self, df):
+    def tokenization(self):
         #tokenizing the entire tweet's text
         #example: "I like dog" becomes ["I", "like", "dog"]
         def tokens(text):
@@ -81,7 +80,7 @@ class SentimentAnalyzer:
             return text
         self.df['Tokenized'] = self.df['Tweet'].apply(lambda x: tokens(x.lower()))
 
-    def lemmatizer(self, df):
+    def lemmatizer(self):
         #lemmatization-capable machine would know that “studies” is the singular verb form of the word “study” in the present tense.
         wn = nltk.WordNetLemmatizer()
         def lemmatizor(text):
@@ -89,15 +88,40 @@ class SentimentAnalyzer:
             return text
         self.df['Lemmatized'] = self.df['Tokenized'].apply(lambda x: lemmatizor(x))
 
-    def sentiment_score_generator(self, df):
+    def sentiment_score_generator(self):
         #Generating the Sentiment Score
         #The compound score generated indicates the level of polarity towards positive and Negative sentiment.
         #If a compound score is more towards -1, then the tweet has a strong negative sentiment.
         #If a compound score is more towards +1, then the tweet has a strong positive sentiment.
         sid = SIA()
-        self.df['sentiments'] = self.df["Tweet"].apply(lambda x: sid.polarity_scores(' '.join(re.findall(r'\w+',str(x).lower()))))
-        self.df['Positive Sentiment'] = self.df['sentiments'].apply(lambda x: x['pos']+1*(10**-6))
-        self.df['Neutral Sentiment'] = self.df['sentiments'].apply(lambda x: x['neu']+1*(10**-6))
-        self.df['Negative Sentiment'] = self.df['sentiments'].apply(lambda x: x['neg']+1*(10**-6))
+        self.sentiment_scores = self.df["Tweet"].apply(lambda x: sid.polarity_scores(' '.join(re.findall(r'\w+',str(x).lower()))))
+        self.sentiment_scores = sid.polarity_scores(' '.join(re.findall(r'\w+',str(self.df["Tweet"][0]).lower())))
+        # self.df['Positive Sentiment'] = self.df['sentiments'].apply(lambda x: x['pos']+1*(10**-6))
+        # self.df['Neutral Sentiment'] = self.df['sentiments'].apply(lambda x: x['neu']+1*(10**-6))
+        # self.df['Negative Sentiment'] = self.df['sentiments'].apply(lambda x: x['neg']+1*(10**-6))
 
-        print(self.df)
+    def run_sentiment_analysis(self):
+        """
+        Runs the whole sentiment analysis pipeline using the above functions.
+        Does not return anything.
+        """
+        self.url_removal()
+        self.punctuation_removal()
+        self.single_and_double_character_space_removal()
+        self.stopwords_removal()
+        self.remove_emoji()
+        self.tokenization()
+        self.lemmatizer()
+        self.sentiment_score_generator()
+
+
+    def get_sentiment_score(self):
+            """
+            :return: A three tuple (positive score, neutral score, negative score)
+            """
+            # pos_score = float(self.df['Positive Sentiment'])
+            # neut_score = float(self.df['Neutral Sentiment'])
+            # neg_score = float(self.df['Negative Sentiment'])
+            # return (pos_score, neut_score, neg_score)
+            return self.sentiment_scores
+
