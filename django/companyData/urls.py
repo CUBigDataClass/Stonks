fed9be329b228e32cb9a121f9fee3808f077cee2
chithihuynh <chithihuynh@gmail.com>
2022-04-15 17:33:50 -0600

    stock fields flatten

--- django/companyData/urls.py
+++ django/companyData/urls.py
@@ -1,15 +1,36 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
-
-# URLConf
+# https://docs.djangoproject.com/en/4.0/topics/http/urls/
+# # URLConf
 urlpatterns = [
-    path('company/<str:company_ticker>/<str:days>/', views.getCompanyDataStr),
-    path('company/<str:company_ticker>/<int:days>/', views.getCompanyDataStr),
-    path('company/<str:company_ticker>/<int:days>/', views.getCompanyData),
-    path('company/<str:company_ticker>/<str:days>', views.getCompanyDataStr),
-    path('company/<str:company_ticker>/<int:days>', views.getCompanyData),
-    path('company/<str:company_ticker>/', views.getCompanyData),
-    path('company/<str:company_ticker>', views.getCompanyData),
+    path('<str:company_ticker>/<int:days>/', include([
+        path('stock/open/', views.getStock, {'fields':['open']}),
+        path('stock/close/', views.getStock, {'fields':['close']}),
+        path('stock/low/', views.getStock, {'fields':['low']}),
+        path('stock/high/', views.getStock, {'fields':['high']}),
+        path('stock/', views.getStock),
+        path('news/', views.getNews),
+        path('tweets/',views.getTweets),
+        path('companyInfo/', views.getCompanyInfo),
+        ])),
     path('company/', views.listCompanies),
 ]
+
+
+# urlpatterns = [
+#     path('company/<str:company_ticker>/<int:days>/stock/open/', views.getStockOpen),
+#     # path('company/<str:company_ticker>/<int:days>/stock/close/', views.getStockClose),
+#     # path('company/<str:company_ticker>/<int:days>/stock/high/', views.getStockHigh),
+#     # path('company/<str:company_ticker>/<int:days>/stock/low/', views.getStockLow),
+#     # path('company/<str:company_ticker>/<int:days>/', views.getCompanyData),
+#     # path('company/<str:company_ticker>/<int:days>', views.getCompanyData),
+#     # path('company/<str:company_ticker>/<str:days>', views.getCompanyDataStr),
+#     # path('company/<str:company_ticker>/<str:days>/', views.getCompanyDataStr),
+#     # path('company/<str:company_ticker>/<str:days>', views.getCompanyDataStr),
+#     # path('company/<str:company_ticker>/<int:days>/', views.getCompanyData),
+#     # path('company/<str:company_ticker>/<int:days>', views.getCompanyData),
+#     path('company/<str:company_ticker>/', views.getCompanyData),
+#     path('company/<str:company_ticker>', views.getCompanyData),
+#     path('company/', views.listCompanies),
+# ]
--- django/companyData/views.py
+++ django/companyData/views.py
@@ -2,6 +2,7 @@ import csv
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
+from django.core import serializers
 
 # from .models import Companies, News, Stocks, Tweets
 # from .models import Companies, News, Stocks, Tweets
@@ -18,11 +19,73 @@ from django.http import QueryDict
 # Create your views here.
 
 
+def getStock(request, company_ticker='AAPL', days=365,fields=['open', 'close','low','high']):
+    date_range = timedelta(days=days)
+    to_date = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    from_date = to_date - date_range
+    
+    # # Get filtered data
+    
+    # https://d3-graph-gallery.com/graph/dendrogram_basic.html
+    # https://d3-graph-gallery.com/graph/treemap_json.html
+    # https://stackoverflow.com/questions/15874233/output-django-queryset-as-json
+    data ={}
+    data['date'] = list( Stocks.objects.filter(pk=company_ticker).filter(date__range=(from_date, to_date)).values_list('date',flat=True))
+    
+    for field in fields:
+        data[field] = list( Stocks.objects.filter(pk=company_ticker).filter(date__range=(from_date, to_date)).values_list(field,flat=True))
+    
+    return JsonResponse({'stock': data})
+
+def getNews(request, company_ticker='AAPL', days=365):
+    date_range = timedelta(days=days)
+    to_date = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    from_date = to_date - date_range
+    
+    # # Get filtered data
+
+    query_set = News.objects.filter(company_ticker__contains=company_ticker).filter(date_published__range=(from_date, to_date)).order_by(
+        '-date_published').values('date_published', 'title', 'author', 'article_url', 'url_image', 'article_description', 'company_ticker')
+
+    # data = {}
+    # fields = ['date_published', 'title', 'author', 'article_url', 'url_image', 'article_description', 'company_ticker']
+    # for field in fields:
+    #     query_set = News.objects.filter(company_ticker__contains=company_ticker).filter(date_published__range=(from_date, to_date)).order_by('-date_published').values_list(field, flat=True)
+    #     data[field] = list( ewNs.objects.filter(company_ticker__contains=company_ticker).filter(date__range=(from_date, to_date)).values_list(field,flat=True))
+    
+
+    num_articles_ret = 5
+    list1 = list(query_set)
+    data = list1[0:num_articles_ret]
+    
+    return JsonResponse({'news': data})
+
+def getTweets(request, company_ticker='AAPL', days=365):
+    date_range = timedelta(days=days)
+    to_date = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    from_date = to_date - date_range
+    
+    query_set = Tweets.objects.filter(company_ticker__contains=company_ticker).filter(
+        date_published__range=(from_date, to_date)).values('date_published', 'sentiment','compound_score')
+    data = getSentiment(query_set)
+    return JsonResponse({'tweets': data})
+    # return JsonResponse(data, save=False)
+    
+def getCompanyInfo(request, company_ticker='AAPL'):
+
+    data = list(Companies.objects.filter(company_ticker__contains=company_ticker).values())
+    return JsonResponse({'company_info': data})
+
+    
+
+    
+
 # (OLD) query string: url = 'http://127.0.0.1:8000/companyData/company/TICKER?q=7' ## where q=7,14,31
 # (NEW TO USE): query string:
 # url = 'http://127.0.0.1:8000/companyData/company/TICKER/7'
 # url = 'http://127.0.0.1:8000/companyData/company/TICKER/14'
 # url = 'http://127.0.0.1:8000/companyData/company/TICKER/365'
+
 def getCompanyDataStr(request, company_ticker='AAPL', days='365'):
     days = int(days)
     return getCompanyData(request, company_ticker, days)
